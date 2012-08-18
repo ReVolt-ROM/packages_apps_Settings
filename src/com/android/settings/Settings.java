18c2e33af27e3736190d9a0e1b8d74fdb778c7f5
Danesh M <daneshm90@gmail.com>
2012-08-18 13:46:57 -0400

    Settings: Forward port CM Screen Security settings (Part 1 of 2)
    includes:
    - Menu Unlock, Quick Unlock, Home unlock and Slide lock delay settings
      Author: DvTonder
    
    - Lockscreen cleanup: Rework the logic to separate cm lockscreen
      settings from stock lockscreen settings.
      Author: Danesh M
      Id: Ic3eebc30fbb55afa6254889b17bde6e3083d1847
    
    - Fix unknown sources crash
      Modify logic to only make cmsecurity specify the extra,
      any other time, default to stock security
      Author: Danesh M
      Id: Ida4b8ccc2f41d79aa9013029b4245ae4d3cd4496
    
    - Adjust available options based on screen lock picked
      Disable / enable the available screen security aditional options based
      on what screen lock was picked.
      Author: jfassad
      Id: I634b18b58d0e1a1c075f9808776fc4f685316221
    
    - Add home button unlock option
      Author: invisiblek
      Id: I10b96b2f788ca26bc45f1758e612b9dfe7beff59
    
    Change-Id: I0dda524830d8dce571910eb1fbd66d5b5af11bff
    
    Conflicts:
    
    	res/values/strings.xml
    	src/com/android/settings/SecuritySettings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -869,4 +869,5 @@ public class Settings extends PreferenceActivity
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
     public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
+    public static class RevoltActivity extends Settings { /* empty */ }
 }

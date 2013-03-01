0f1b6ef196199f4745879645fe7a80abf080e904
Bajee11 <bajee11@gmail.com>
2013-03-01 08:03:27 -0500

    QuietHours Toggle (2/2)
    Required for longpress action.
    
    Reflect change in Settings.
    
    Signed-off-by: Bajee11 <bajee11@gmail.com>
    Change-Id: I2082e12fb2ce9e4063d960df540259eb6822fbcd
    
    Conflicts:
    
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -868,4 +868,5 @@ public class Settings extends PreferenceActivity
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class ProfilesSettingsActivity extends Settings { /* empty */ }
+    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
 }

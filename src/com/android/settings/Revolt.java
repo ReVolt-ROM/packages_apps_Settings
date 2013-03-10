cda224ecbe0b685d0a9a91c82ddb054d29bf67a0
John Hany <johnhany97@gmail.com>
2013-03-10 21:06:41 +0200

    Rest of Notification Behaviour
    Change-Id: I2d4cbf2939e1c3ecf4766fbd8e437c223434ad4a

--- src/com/android/settings/Revolt.java
+++ src/com/android/settings/Revolt.java
@@ -44,9 +44,13 @@ public class Revolt extends SettingsPreferenceFragment implements
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_FONT_SIZE = "font_size";
     private static final String KEY_EXPANDED_DESKTOP = "power_menu_expanded_desktop";
+    private static final String KEY_NOTIFICATION_BEHAVIOUR = "notifications_behaviour";
     
     private CheckBoxPreference mExpandedDesktopPref;
+    private ListPreference mNotificationsBeh;
     private final Configuration mCurConfig = new Configuration();
+    private ContentResolver mCr;
+    private PreferenceScreen mPrefSet;
 
     public boolean hasButtons() {
         return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
@@ -55,8 +59,16 @@ public class Revolt extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mPrefSet = getPreferenceScreen();
+        mCr = getContentResolver();
 
         addPreferencesFromResource(R.xml.revolt_customizations);
+
+        int CurrentBeh = Settings.Secure.getInt(mCr, Settings.Secure.NOTIFICATIONS_BEHAVIOUR, 0);
+        mNotificationsBeh = (ListPreference) findPreference(KEY_NOTIFICATION_BEHAVIOUR);
+        mNotificationsBeh.setValue(String.valueOf(CurrentBeh));
+                mNotificationsBeh.setSummary(mNotificationsBeh.getEntry());
+        mNotificationsBeh.setOnPreferenceChangeListener(this);
 
         mExpandedDesktopPref = (CheckBoxPreference) findPreference(KEY_EXPANDED_DESKTOP);
         boolean showExpandedDesktopPref =
@@ -69,6 +81,7 @@ public class Revolt extends SettingsPreferenceFragment implements
             mExpandedDesktopPref.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 0) == 1));
         }
+
 
         // Do not display lock clock preference if its not installed
         removePreferenceIfPackageNotInstalled(findPreference(KEY_LOCK_CLOCK));
@@ -121,6 +134,13 @@ public class Revolt extends SettingsPreferenceFragment implements
                 Log.e(TAG, "package " + packageName + " not installed, hiding preference.");
                 getPreferenceScreen().removePreference(preference);
                 return true;
+        } else if (preference == mNotificationsBeh) {
+            String val = (String) Value;
+                     Settings.Secure.putInt(mCr, Settings.Secure.NOTIFICATIONS_BEHAVIOUR,
+            Integer.valueOf(val));
+            int index = mNotificationsBeh.findIndexOfValue(val);
+            mNotificationsBeh.setSummary(mNotificationsBeh.getEntries()[index]);
+            return true;
             }
         }
         return false;

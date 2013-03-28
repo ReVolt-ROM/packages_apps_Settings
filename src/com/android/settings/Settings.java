db2f113d01e6ce88a68bee1098b2f5ec572d86ff
John Hany <johnhany97@gmail.com>
2013-03-28 22:53:43 +0200

    FIXES

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -885,6 +885,4 @@ public class Settings extends PreferenceActivity
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class ProfilesSettingsActivity extends Settings { /* empty */ }
     public static class PieActivity extends Settings { /* empty */ }
-    public static class ApnSettingsActivity extends Settings { /* empty */ }
-    public static class ApnEditorActivity extends Settings { /* empty */ }
 }

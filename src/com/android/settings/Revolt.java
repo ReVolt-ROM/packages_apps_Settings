96317fe5a7413d16812c4b9767daa5cc1a981d30
John Hany <johnhany97@gmail.com>
2013-04-21 14:53:16 +0300

    CHANGES

--- src/com/android/settings/Revolt.java
+++ src/com/android/settings/Revolt.java
@@ -52,7 +52,7 @@ public class Revolt extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "Revolt";
 
-    private static final String KEY_LOCK_CLOCK = "lock_clock";
+    //private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_FONT_SIZE = "font_size";
     private static final String KEY_EXPANDED_DESKTOP = "power_menu_expanded_desktop";
     
@@ -88,10 +88,10 @@ public class Revolt extends SettingsPreferenceFragment implements
         }
 
         // Do not display lock clock preference if its not installed
-        removePreferenceIfPackageNotInstalled(findPreference(KEY_LOCK_CLOCK));
+        //removePreferenceIfPackageNotInstalled(findPreference(KEY_LOCK_CLOCK));
 
         // Do not display font size preference if its not installed
-        removePreferenceIfPackageNotInstalled(findPreference(KEY_FONT_SIZE));
+        //removePreferenceIfPackageNotInstalled(findPreference(KEY_FONT_SIZE));
     
     }
 

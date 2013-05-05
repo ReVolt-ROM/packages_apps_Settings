167108be722bf37d1448abcd758543829f801525
Danesh Mondegarian <daneshm90@gmail.com>
2013-05-05 16:29:39 -0400

    ProfilesSettings : Allow nfc override (2/2)
    Allow user to override nfc state via profiles
    
    Change-Id: I2a401f17ce42b1711588242e0604997e1f5d8399
    
    Conflicts:
    
    	res/values/cm_strings.xml

--- src/com/android/settings/profiles/ProfileConfig.java
+++ src/com/android/settings/profiles/ProfileConfig.java
@@ -92,11 +92,12 @@ public class ProfileConfig extends SettingsPreferenceFragment
 
         mConnections = new ArrayList<ConnectionItem>();
         mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_MOBILEDATA, getString(R.string.toggleData)));
-        mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_BLUETOOTH, getString(R.string.toggleBluetooth)));
+        mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_BLUETOOTH, getString(R.string.toggleBluetooth))); 
         mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_GPS, getString(R.string.toggleGPS)));
         mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_WIFI, getString(R.string.toggleWifi)));
         mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_SYNC, getString(R.string.toggleSync)));
-        mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_WIFIAP, getString(R.string.toggleWifiAp)));
+	mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_WIFIAP, getString(R.string.toggleWifiAp)));
+        mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_NFC, getString(R.string.toggleNfc)));
         if (WimaxHelper.isWimaxSupported(getActivity())) {
             mConnections.add(new ConnectionItem(ConnectionSettings.PROFILE_CONNECTION_WIMAX, getString(R.string.toggleWimax)));
         }
@@ -439,3 +440,4 @@ public class ProfileConfig extends SettingsPreferenceFragment
         }
     }
 }
+

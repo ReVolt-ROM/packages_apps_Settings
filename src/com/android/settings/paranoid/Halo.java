53958d24548bffda02f292dd181235b25690ba58
Daniel MM <daxx4182@gmail.com>
2013-06-15 18:53:36 +0800

    HALO Style just got lil better
    Conflicts:
    
    	res/values/arrays.xml
    	src/com/android/settings/paranoid/Halo.java

--- src/com/android/settings/paranoid/Halo.java
+++ src/com/android/settings/paranoid/Halo.java
@@ -56,11 +56,11 @@ public class Halo extends SettingsPreferenceFragment
     private static final String PREF_HALO_STYLE = "halo_style";
 
     private ListPreference mHaloState;
+    private ListPreference mHaloStyle;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
     private CheckBoxPreference mHaloPause;
     private CheckBoxPreference mHaloEnabled;
-    private CheckBoxPreference mHaloStyle;
 
     private Context mContext;
     private INotificationManager mNotificationManager;
@@ -98,9 +98,8 @@ public class Halo extends SettingsPreferenceFragment
         mHaloPause.setChecked(Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_PAUSE, isLowRAM) == 1);
 
-        mHaloStyle = (CheckBoxPreference) findPreference(PREF_HALO_STYLE);
-        mHaloStyle.setChecked(Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_STYLE, 0) == 1);
+        mHaloStyle = (ListPreference) findPreference(PREF_HALO_STYLE);
+        mHaloStyle.setOnPreferenceChangeListener(this);
         }
 
     private boolean isHaloPolicyBlack() {
@@ -130,11 +129,6 @@ public class Halo extends SettingsPreferenceFragment
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_PAUSE, mHaloPause.isChecked()
                     ? 1 : 0);
-        } else if (preference == mHaloStyle) {
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.HALO_STYLE, mHaloStyle.isChecked()
-                    ? 1 : 0);
-            Helpers.restartSystemUI();
         }	
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
@@ -147,6 +141,12 @@ public class Halo extends SettingsPreferenceFragment
             } catch (android.os.RemoteException ex) {
                 // System dead
             }          
+            return true;
+        } else if (preference == mHaloStyle) {
+            int val = Integer.valueOf((String) Value);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.HALO_STYLE, val);
+            Helpers.restartSystemUI();
             return true;
         }
         return false;

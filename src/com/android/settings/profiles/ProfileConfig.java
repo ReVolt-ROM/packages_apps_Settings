7f14ce3b22e64b4bdd7d6e126af25cf3c15b1892
tpoint75 <tpoint75@googlemail.com>
2013-06-16 14:49:39 +0200

    fix build

--- src/com/android/settings/profiles/ProfileConfig.java
+++ src/com/android/settings/profiles/ProfileConfig.java
@@ -34,6 +34,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.nfc.NfcAdapter;
 import android.net.wimax.WimaxHelper;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -132,7 +133,7 @@ public class ProfileConfig extends SettingsPreferenceFragment
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        if (deviceSupportsNfc(getActivity())) {
+        if (NfcAdapter.getDefaultAdapter(getActivity()) != null) {
             MenuItem nfc = menu.add(0, MENU_NFC_WRITE, 0, R.string.profile_write_nfc_tag)
                 .setIcon(R.drawable.ic_menu_nfc_writer_dark);
             nfc.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |

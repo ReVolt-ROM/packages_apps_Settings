d62b4acd9e34e48874e3ed3949acddba85a8dc50
John Hany <johnhany97@gmail.com>
2013-07-17 18:57:14 +0300

    Fix commit

--- src/com/android/settings/profiles/ProfileConfig.java
+++ src/com/android/settings/profiles/ProfileConfig.java
@@ -199,7 +199,7 @@ public class ProfileConfig extends SettingsPreferenceFragment
     }
 
     private void startTriggerFragment() {
-        final PreferenceActivity pa = (PreferenceActivity) getActivity();
+        final PreferenceDrawerActivity pa = (PreferenceDrawerActivity) getActivity();
         final Bundle args = new Bundle();
         args.putParcelable("profile", mProfile);
 

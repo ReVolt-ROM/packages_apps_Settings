73869ddf0f99e8d284a942f8b9c8dc6dd8e05342
tpoint75 <tpoint75@googlemail.com>
2013-09-01 21:44:17 +0200

    fix build

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -70,7 +70,7 @@ import com.android.settings.users.ProfileUpdateReceiver;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter
+import java.io.FileWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;

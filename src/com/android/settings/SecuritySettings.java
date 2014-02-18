7149c1f889827dd5945f1369e9567670ea6e082f
DvTonder <david.vantonder@gmail.com>
2014-02-18 20:56:40 +0530

    [5/7] Settings: Blacklist support
    Base: https://gerrit.omnirom.org/#/c/5668/
    telephony: https://gerrit.omnirom.org/#/c/6347/
    InCallUI: https://gerrit.omnirom.org/#/c/5669/
    Mms: https://gerrit.omnirom.org/#/c/5671/
    TelephonyProvider: https://gerrit.omnirom.org/#/c/5012/
    Telephony: https://gerrit.omnirom.org/#/c/5010/
    
    Change-Id: I6739bbd72f64095a324f6c6188e2eac53a2783b9
    
    [5/7] Settings: Blacklist permission
    
    Change-Id: I68ade9b1aeea4262c1937b774c504419c96fabae
    
    Conflicts:
    
    	res/values-cs/strings_custom.xml

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -43,7 +43,9 @@ import android.security.KeyStore;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.android.internal.telephony.util.BlacklistUtils;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -89,6 +91,10 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String KEY_NOTIFICATION_ACCESS = "manage_notification_access";
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
 
+    // Omni Additions
+    private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
+    private static final String KEY_BLACKLIST = "blacklist";
+
     private PackageManager mPM;
     private DevicePolicyManager mDPM;
 
@@ -121,6 +127,9 @@ public class SecuritySettings extends RestrictedSettingsFragment
     public SecuritySettings() {
         super(null /* Don't ask for restrictions pin on creation. */);
     }
+
+    // Omni Additions
+    private PreferenceScreen mBlacklist;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -352,6 +361,14 @@ public class SecuritySettings extends RestrictedSettingsFragment
             }
         }
 
+        mBlacklist = (PreferenceScreen) root.findPreference(KEY_BLACKLIST);
+
+        // Determine options based on device telephony support
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            // No telephony, remove dependent options
+            root.removePreference(mBlacklist);
+        }
+
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
         if (mNotificationAccess != null) {
             final int total = NotificationAccessSettings.getListenersCount(mPM);
@@ -558,6 +575,8 @@ public class SecuritySettings extends RestrictedSettingsFragment
         if (mEnableKeyguardWidgets != null) {
             mEnableKeyguardWidgets.setChecked(lockPatternUtils.getWidgetsEnabled());
         }
+
+        updateBlacklistSummary();
     }
 
     @Override
@@ -692,5 +711,15 @@ public class SecuritySettings extends RestrictedSettingsFragment
         Intent intent = new Intent();
         intent.setClassName("com.android.facelock", "com.android.facelock.AddToSetup");
         startActivity(intent);
+    }
+
+    private void updateBlacklistSummary() {
+        if (mBlacklist != null) {
+            if (BlacklistUtils.isBlacklistEnabled(getActivity())) {
+                mBlacklist.setSummary(R.string.blacklist_summary);
+            } else {
+                mBlacklist.setSummary(R.string.blacklist_summary_disabled);
+            }
+        }
     }
 }
